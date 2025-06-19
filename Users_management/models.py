2a73b018df476c3acb0c0aa3a1fdabb8aa3ce4c8
Caro-D54 <c.donaire@alumno.um.edu.ar>
2025-06-19 20:28:18 -0300

    Eliminando superuser

--- Users_management/models.py
+++ Users_management/models.py
@@ -19,7 +19,6 @@ class User(AbstractBaseUser):
     mail = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'correo'
     REQUIRED_FIELDS = ['nombre']
