62dc8c2375ec718ca5c2e2ef6b12f6b339abcb00
Caro-D54 <c.donaire@alumno.um.edu.ar>
2025-11-14 01:10:03 +0100

    Preparando para conectar con front

--- Book_reserve/settings.py
+++ Book_reserve/settings.py
@@ -146,7 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:8000",
+]
+
+CORS_ALLOWED_CREDENTIALS = [
+    "true"
 ]
 
 AUTH_USER_MODEL = 'Users_management.User' 
